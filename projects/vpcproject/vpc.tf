2567413cb22f33f0a9966a0d293da3473402311a
Sekar <ctrwillow@gmail.com>
2023-03-07 13:53:18 -0600

    public-private-subnets-routetables-associations-created

--- projects/vpcproject/vpc.tf
+++ projects/vpcproject/vpc.tf
@@ -5,5 +5,10 @@ module "vpc" {
   vpc_cidr_block  = var.vpc_cidr_block
   aws_projectname = var.aws_projectname
   aws_region      = var.aws_region
-
+  public_subnet_az1_cidr =  var.public_subnet_az1_cidr
+  public_subnet_az2_cidr =  var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr =  var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
 }
